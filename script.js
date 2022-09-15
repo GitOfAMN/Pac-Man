426d156471610323a4e0e4aec6ca6e177e9df04a
OliveOyl89 <ashleynickens@gmail.com>
2022-09-15 10:35:20 -0400

    For review

--- script.js
+++ script.js
@@ -1,34 +1,42 @@
-// Calling the variables in HTML that are needed to reference in JS
+/***************************
+ CACHE DOM NODES
+ ***************************/
 
-const modalOne = document.querySelector('.modal-one'),
-      modalTwo = document.querySelector('.modal-two'),
-      button = document.querySelector('button')
-
-const gamePage = document.querySelector('.game-page')
-const startGame = document.querySelector('.lets-play')
+const openModalOne = document.querySelector('.get-started')
+const openGamePage = document.querySelector('.lets-play')
+const gamePage = document.querySelector('.gameboard')
 
 const canvas = document.querySelector('canvas')
 const context = canvas.getContext('2d')
 console.log(context)
 
+/***************************
+UTILITY FUNCTIONS & OTHER VARIABLES
+ ***************************/
+
 canvas.width = innerWidth
 
 
-// Calling the functions to effect the abovementioned variables
+const setShownModal = (modalNumber) => {
+    const modals = document.querySelectorAll('.modal')
+    modals.forEach((modal) => {
+        modal.style.display = 'none'
+    })
 
-button.addEventListener('click', (evt) => {
-  modalOne.classList.add('hide');
-  modalTwo.classList.remove('hide');
-})
+    modalNumber ? document.getElementById(modalNumber).style.display = 'block' : null
 
-startGame.addEventListener('click', (evt) => {
-    modalTwo.classList.add('hide');
-    gamePage.classList.remove('hide');
-})
+    document.getElementById(modalNumber).style.display = 'block'
+}
+
+/***************************
+ CLASSES
+ ***************************/
 
 // Game Board
 
 class Boundary {
+    static width = 40
+    static height = 40
     constructor({ position } ) {
        this.position = position
        this.width = 40
@@ -41,22 +49,83 @@ class Boundary {
     }
 }
 
-const boundaries = [
-    new Boundary({
-        position: {
-            x: 0,
-            y: 0
-        }
-    }),
+const map = [
+    ['-', '-', '-', '-', '-', '-', ],
+    ['-', ' ', ' ', ' ', ' ', '-', ],
+    ['-', ' ', '-', '-', ' ', '-', ],
+    ['-', ' ', ' ', ' ', ' ', '-', ],
+    ['-', '-', '-', '-', '-', '-', ]
+]
+const boundaries = []
 
-    new Boundary({
-        position: {
-            x: 41,
-            y: 0
+map.forEach((row, i) => {
+    row.forEach((symbol, j) => {
+        switch (symbol) {
+           case '-':
+            boundaries.push(
+                new Boundary({
+                    position: {
+                        x: Boundary.width * j,
+                        y: Boundary.height * i
+                    }
+                })
+            )
+            break
         }
     })
-]
+})
 
 boundaries.forEach((boundary) => {
     boundary.draw()
 })
+
+// The Pac-Man
+
+class PacMan {
+    constructor() {
+       this.position = position
+       this.velocity = velocity
+       this.radius = 10
+        
+    }
+
+    draw() {
+        context.beginPath()
+        context.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2)
+        context.fillStyle = 'yellow'
+        context.fill()
+        context.closePath()
+    }
+}
+
+
+
+/***************************
+EVENT LISTENERS
+ ***************************/
+
+/*
+ When the button in the first modal is clicked show the second Modal and make all others dissapear
+*/
+document.querySelector('#one > button')
+  .addEventListener('click', (evt) => {
+    setShownModal('two')
+  })
+
+document.querySelector('#two > button')
+  .addEventListener('click', (evt) => {
+    setShownModal('three')
+  })
+
+document.querySelector('.gamrboard > button')
+  .addEventListener('click', (evt) => {
+    setShownModal('gameboard')
+  })
+
+/* When the Button is clicked that is in the final modal make all modals disappear and make the game board visible
+  */
+
+document.querySelector('#three > button')
+    .addEventListener('click', (evt) => {
+    setShownModal(null)
+  })
